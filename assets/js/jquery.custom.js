994ccfe7f4ee8ebcc4a93a6c5c79a541b6c0aa68
Greg Schafer <grschafer@gmail.com>
2012-01-24 07:53:34 -0600

    Removed images, added page titles, added omgpop page

--- assets/js/jquery.custom.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-
-	$("#featured-projects a img, .project-img, .project-img").hover(function() {
-		$(this).animate({ opacity: 0.7 }, 300);
-	}, function() {
-		$(this).animate({ opacity: 1 }, 300);
-	});
-		
-})(jQuery);
